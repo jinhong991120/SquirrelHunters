4414dcf984f592255fe9701d7c12b1c36db06217
bjs14 <bjs14@asb9700u-g06.csil.sfu.ca>
2023-11-01 15:21:38 -0700

    Potato class

---
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/Potato.java
@@ -0,0 +1,14 @@
+/**
+ * Implementation of GameFeature class for Potato 
+ */
+class Potato implements GameFeature{
+    /**Random generation of Acorn */
+    public void rand_generate(){
+
+    }
+    /**Generates Acorn in specific spot */
+    public void generate(int x_pos, int y_pos){
+
+    }
+
+}
