d1c6a7427906eae215a4d53859767af7f405e7d3
bjs14 <bjs14@asb9700u-g06.csil.sfu.ca>
2023-11-01 15:08:33 -0700

    Acorn class

---
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/Acorn.java
@@ -0,0 +1,14 @@
+/**
+ * Implementation of GameFeature for Acorn
+ */
+public class Acorn implements GameFeature{
+    /** Random generation of Acorn */
+    public void rand_generate(){
+        
+    }
+    /**Generates Acorn in specific spot */
+    public void generate(int x_pos, int y_pos){
+
+    }
+    
+}
