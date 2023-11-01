a3dc5bf1316696693a4a95a46e14428b57693b98
Jin Hong <jin_hong@sfu.ca>
2023-10-31 22:46:05 -0700

    Create GamePanel.java

---
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/GamePanel.java
@@ -0,0 +1,47 @@
+package com.group11.game;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D; 
+import java.awt.Color;
+/**
+ * The GamePanel class represents the main panel for a simple game.
+ * It contains all the game logic and rendering, and handles user input.
+ */
+public class GamePanel extends JPanel implements Runnable{
+    
+    /**
+     * The size of each tile (Deafult: 32)
+     */
+    protected final int tileSize = 32;
+
+    /**
+     * The number of columns (horizontal tiles) on the map (Deafult: 40)
+     */
+    protected final int maxScreenCol = 40;
+
+    /**
+     * The number of rows (vertical tiles) on the map (Deafult: 24)
+     */
+    protected final int maxScreenRow = 24;
+
+    /**
+     * Screen Width (Default: 1280 pixels)
+     */
+    protected final int screenWidth = tileSize * maxScreenCol;    
+
+    /**
+     * Screen Height (Default: 768 pixels)
+     */
+    protected final int screenHeight = tileSize * maxScreenRow;   // 768 pixels
+    
+    /**
+     * Frames per Seconds (Deafult: 60)
+     */
+    protected final int FPS = 60;
+
+    /**
+     * Game {@code Thread}
+     */
+    protected Thread gameThread;
