b2e0b9ebbca334c41afc87719a62f183e3fa56ee
Jefferson Li <jla788@sfu.ca>
2023-11-07 15:21:22 -0800

    Acorn class still in progress

--- MVN_PJ/phase2-module/src/main/java/com/group11/game/Acorn.java
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/Acorn.java
@@ -1,14 +1,36 @@
 /**
- * Implementation of GameFeature for Acorn
+ * extends item class for Potato 
  */
-public class Acorn implements GameFeature{
-    /** Random generation of Acorn */
-    public void rand_generate(){
-        
-    }
-    /**Generates Acorn in specific spot */
-    public void generate(int x_pos, int y_pos){
+package com.group11.game;
 
+import java.io.IOException;
+
+public class Acorn extends Items{
+    public Acorn() {
+
+        name = "Acorn"; 
+
+        try {
+            image = ImageIO.read()
+        }catch(IOException e) {
+            e.printStackTrace(); 
+        }
+
+        score += 10; 
+
+        xPosition = 0;
+        yPosition = 0; 
+
+        }
     }
     
-}
+    // /** Random generation of Acorn */
+    // public void rand_generate(){
+        
+    // }
+    // /**Generates Acorn in specific spot */
+    // public void generate(int x_pos, int y_pos){
+
+    // }
+    
+//}
