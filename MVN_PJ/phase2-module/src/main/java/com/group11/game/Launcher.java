eb677e5cdcd656f74430100eb87a5bfb6cbcfe7e
Jin Hong <jin_hong@sfu.ca>
2023-11-08 17:57:11 -0800

    Create Launcher.java

---
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/Launcher.java
@@ -0,0 +1,26 @@
+package com.group11.game;
+
+import javax.swing.JFrame;
+
+/**
+ * Game Launcher
+ */
+public class Launcher {
+    public static void main(String[] args){
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Squirrel Hunters");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.setupGame();
+        gamePanel.startGameThread();
+    }
+}
