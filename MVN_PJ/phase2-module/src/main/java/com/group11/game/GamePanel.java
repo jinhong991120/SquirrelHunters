af33886df97d44878bc3bbe62d4899807564989f
Jin Hong <jin_hong@sfu.ca>
2023-11-08 18:03:23 -0800

    Update GamePanel.java

--- MVN_PJ/phase2-module/src/main/java/com/group11/game/GamePanel.java
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/GamePanel.java
@@ -12,67 +12,99 @@ import java.awt.Color;
 public class GamePanel extends JPanel implements Runnable{
     
     /**
-     * The size of each tile (Deafult: 32)
-     */
+    * The size of each tile (Deafult: 32)
+    */
     protected final int tileSize = 32;
 
     /**
-     * The number of columns (horizontal tiles) on the map (Deafult: 40)
-     */
+    * The number of columns (horizontal tiles) on the map (Deafult: 40)
+    */
     protected final int maxScreenCol = 40;
 
     /**
-     * The number of rows (vertical tiles) on the map (Deafult: 24)
-     */
+    * The number of rows (vertical tiles) on the map (Deafult: 24)
+    */
     protected final int maxScreenRow = 24;
 
     /**
-     * Screen Width (Default: 1280 pixels)
-     */
+    * Screen Width (Default: 1280 pixels)
+    */
     protected final int screenWidth = tileSize * maxScreenCol;    
 
     /**
-     * Screen Height (Default: 768 pixels)
-     */
-    protected final int screenHeight = tileSize * maxScreenRow;   // 768 pixels
+    * Screen Height (Default: 768 pixels)
+    */
+    protected final int screenHeight = tileSize * maxScreenRow;  
     
     /**
-     * Frames per Seconds (Deafult: 60)
-     */
+    * Frames per Seconds (Deafult: 60)
+    */
     protected final int FPS = 60;
 
     /**
-     * Game {@code Thread}
-     */
+    * Game {@code Thread}
+    */
     protected Thread gameThread;
     
     /**
-     * Collision Checker
-     */
+    * Collision Checker
+    */
     protected CollisionChecker cChecker = new CollisionChecker(this);
     
     /**
-     * {@code KeyHandler} to interact with keyboard inputs
-     */
+    * {@code KeyHandler} to interact with keyboard inputs
+    */
     protected KeyHandler key = new KeyHandler(this);
     
-     /**
-     * Main {@code Characters} which is the {@code Squirrel}
-     */
+    /**
+    * Main {@code Characters} which is the {@code Squirrel}
+    */
     protected Squirrel squirrel = new Squirrel(this, key);
 
     /**
-     * {@code TileManger}
-     */
+    * {@code TileManger}
+    */
     protected TileRepresenter tr = new TileRepresenter(this);
     
+  
+    
     /**
-     * Rewards {@code Items}
-     */
-    protected Items rewards[] = new Items[10];
+    * {@code Sound} to play music 
+    */
+    protected Sound sound = new Sound();
 
     /**
-     * Punishments {@code Items}
-     */
-    protected Items punishments[] = new Items[10];
+    * {@code UI} on screen
+    */
+    protected UI ui = new UI (this);
+    
 
+    protected Items Acorn[] = new Items[10];
+
+
+    protected Items potato[] = new Items[10];
+
+    /**
+    * Path finder
+    */
+    protected Pathfinder pFinder = new Pathfinder(this);
+
+    /**
+    * Assset Setter
+    */
+    protected Setter setter = new Setter(this);
+
+    /**
+    * Enemy Raccoons {@code Characters}
+    */
+    protected Characters raccoons[] = new Characters[3];
+
+    /**
+    * {@code Portal} to exit after all rewards ({@code Acorn}) are collected
+    */
+    protected Portal portal = new Portal();
+
+    /**
+    * The Game {@code State}
+    */
+    protected State state;
