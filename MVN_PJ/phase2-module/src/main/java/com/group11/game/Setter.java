8d2b38db554496d7b7393e2245676e8789cecb74
Jin Hong <jin_hong@sfu.ca>
2023-11-08 17:54:10 -0800

    Create Setter.java

---
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/Setter.java
@@ -0,0 +1,63 @@
+package com.group11.game;
+
+/**
+ * Setter class
+ * Setting items and raccoons onto GamePanel
+ */
+
+public class Setter {
+
+  /**
+   * target {@code GamePanel} 
+   */
+  private GamePanel gp;
+
+  /**
+   * Constructor.
+   *
+   * @param gp Game panel to be updated
+   */
+  public Setter(GamePanel gp) { this.gp = gp; }
+
+  /**
+   * Sets the items locations.
+   */
+  public void setObject() {
+
+    int XPositions[] = {1, 1, 23, 11, 8, 35, 36, 13, 24};
+    int YPositions[] = {22, 7, 3, 1, 13, 16, 12, 14, 11};
+
+    for (int i = 0; i < 9; i++) {
+      gp.acorns[i] = new Acorn();
+      gp.acorns[i].xPosition = XPositions[i] * gp.tileSize;
+      gp.acorns[i].yPosition = YPositions[i] * gp.tileSize;
+    }
+
+    /* 
+    // set up Potatoes
+    int XPositions2[] = {5, 12, 30, 36};
+    int YPositions2[] = {6, 8, 19, 15};
+    // i<n where n = # of potatoes on the map
+    for (int i = 0; i < 3; i++) {
+      gp.potato[i] = new Potato();
+      gp.potato[i].xPosition = XPositions2[i] * gp.tileSize;
+      gp.potato[i].yPosition = YPositions2[i] * gp.tileSize;
+    }
+     */
+  }
+ 
+
+  /**
+   * Set the Raccoons locations.
+   */
+  public void setRaccoon() {
+    int Xarrs[] = {35, 14, 20};
+    int Yarrs[] = {9, 18, 5};
+
+    for (int i = 0; i < 3; i++) {
+      gp.raccoons[i] = new Raccoon(gp);
+      gp.raccoons[i].xPosition = Xarrs[i] * gp.tileSize;
+      gp.raccoons[i].yPosition = Yarrs[i] * gp.tileSize;
+    }
+  }
+}
