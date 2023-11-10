a8b0257aae7eff3d60017fc5dc4da9bc236a924b
Jefferson Li <jla788@sfu.ca>
2023-11-09 17:40:59 -0800

    updated and completed orange with image needing confimation

--- MVN_PJ/phase2-module/src/main/java/com/group11/game/Orange.java
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/Orange.java
@@ -4,24 +4,25 @@
 package com.group11.game;
 
 import java.io.IOException;
+import java.awt.Graphics2D;
+import javax.imageio.ImageIO;
 
 public class Orange extends Items{
     public Orange() {
-
         name = "Orange"; 
-
         try {
-            image = ImageIO.read(); 
+            image = ImageIO.read(getClass().getResourceAsStream("directory")); 
         }catch(IOException i) {
             i.printStackTrace(); 
         }
+        collision = true;
+    }
 
-        score += 0; 
-
-        xPosition = 0;
-        yPosition = 0; 
-
-
+    public void draw(Graphics2D g2, GamePanel gp){
+        int pos_x = 0; 
+        int pos_y = 0;
+    
+        g2.drawImage(image, pos_x, pos_y, null);
     }
 }
 
