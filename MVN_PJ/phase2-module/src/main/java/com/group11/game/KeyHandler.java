daf3f5c80cd463582cadfd35b20cc383de88bad9
Zekai Li <zekai_li@sfu.ca>
2023-11-09 23:53:48 -0800

    work on keyPress and keyRelease part

--- MVN_PJ/phase2-module/src/main/java/com/group11/game/KeyHandler.java
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/KeyHandler.java
@@ -8,27 +8,104 @@ import java.awt.event.*;
 public class KeyHandler implements KeyListener {
 
     /**
-     * target {@code GamePanel} 
+     * target {@code GamePanel}
      */
     protected GamePanel gp;
 
     /**
      * {@code Squirrel}'s direction (true - facing direction, false otherwise)
      */
-    protected boolean up, down, right, left;
+    protected boolean up, down, right, left, pause;
 
     /**
      * Constructor.
-     * 
+     *
      * @param gp the target GamePanel
      */
-    public KeyHandler(GamePanel gp){
+    public KeyHandler(GamePanel gp) {
         this.gp = gp;
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-        
+
     }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        switch (keyCode){
+            //up
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
+                up = true;
+                break;
+            //down
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
+                down = true;
+                break;
+            //left
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
+                left = true;
+                break;
+            //right
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
+                right = true;
+                break;
+
+            //pause
+            case KeyEvent.VK_ESCAPE:
+                if(!pause){
+                    //turn to pause state
+                }
+                pause = true;
+
+        }
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        switch (keyCode) {
+            //up
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
+                up = false;
+                break;
+            //down
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
+                down = false;
+                break;
+            //left
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
+                left = false;
+                break;
+            //right
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
+                right = false;
+                break;
+
+            //pause
+            case KeyEvent.VK_ESCAPE:
+                pause = false;
+
+        }
+    }
+
+
+}
+
 
 
