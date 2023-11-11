5c00f8e581cbe82e2bd85a1f06ba0b679be7846e
Jin Hong <jin_hong@sfu.ca>
2023-11-10 18:48:19 -0800

    Update KeyHandler.java

--- MVN_PJ/phase2-module/src/main/java/com/group11/game/KeyHandler.java
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/KeyHandler.java
@@ -8,104 +8,175 @@ import java.awt.event.*;
 public class KeyHandler implements KeyListener {
 
     /**
-     * target {@code GamePanel}
+     * target {@code GamePanel} 
      */
     protected GamePanel gp;
 
     /**
-     * {@code Squirrel}'s direction (true - facing direction, false otherwise)
+     * {@code Student}'s direction (true - facing direction, false otherwise)
      */
-    protected boolean up, down, right, left, pause;
+    protected boolean up, down, right, left;
 
     /**
      * Constructor.
-     *
+     * 
      * @param gp the target GamePanel
      */
-    public KeyHandler(GamePanel gp) {
+    public KeyHandler(GamePanel gp){
         this.gp = gp;
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-
+        
     }
 
-    /**
-     * @param e the event to be processed
-     */
     @Override
     public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        switch (keyCode){
-            //up
-            case KeyEvent.VK_UP:
-            case KeyEvent.VK_W:
-                up = true;
-                break;
-            //down
-            case KeyEvent.VK_DOWN:
-            case KeyEvent.VK_S:
-                down = true;
-                break;
-            //left
-            case KeyEvent.VK_LEFT:
-            case KeyEvent.VK_A:
-                left = true;
-                break;
-            //right
-            case KeyEvent.VK_RIGHT:
-            case KeyEvent.VK_D:
-                right = true;
-                break;
 
-            //pause
-            case KeyEvent.VK_ESCAPE:
-                if(!pause){
-                    gp.state = State.Pause;
-                }
-                pause = true;
+        int code = e.getKeyCode();
 
+        inputTitleScreen(code);
+        inputPause(code);
+        inputMove(code);
+        inputGameOver(code);
+    }
+
+    /**
+     * interact with key code to move the options to choose in title screen
+     * 
+     * @param code key code for input key
+     */
+    public void inputTitleScreen(int code){
+        // Input for buttons on Title screen
+        if(gp.state == State.Title) {
+            switch (code){
+                case KeyEvent.VK_UP:
+                    gp.ui.order--;
+                    if(gp.ui.order < 0){
+                        gp.ui.order = 1;
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    gp.ui.order++;
+                    if(gp.ui.order > 1){
+                        gp.ui.order = 0;
+                    }
+                    break;
+                case KeyEvent.VK_ENTER:
+                    if(gp.ui.order == 0){
+                        gp.state = State.Game;
+                        gp.stopMusic();
+                        gp.playMusic(0);
+                    }
+                    if(gp.ui.order == 1){
+                        System.exit(0);
+                    }
+            }
         }
     }
 
     /**
-     * @param e the event to be processed
+     * interact with key code to do pause
+     * 
+     * @param code key code for input key
      */
-    @Override
-    public void keyReleased(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        switch (keyCode) {
-            //up
-            case KeyEvent.VK_UP:
-            case KeyEvent.VK_W:
-                up = false;
+    public void inputPause(int code){
+        switch(code){
+            case KeyEvent.VK_P:
+                if (gp.state == State.Game) {
+                    gp.state = State.Pause;
+                }
+                else if (gp.state == State.Pause) {
+                    gp.state = State.Game;
+                }
                 break;
-            //down
-            case KeyEvent.VK_DOWN:
-            case KeyEvent.VK_S:
-                down = false;
-                break;
-            //left
-            case KeyEvent.VK_LEFT:
-            case KeyEvent.VK_A:
-                left = false;
-                break;
-            //right
-            case KeyEvent.VK_RIGHT:
-            case KeyEvent.VK_D:
-                right = false;
-                break;
-
-            //pause
             case KeyEvent.VK_ESCAPE:
-                pause = false;
-
+                if (gp.state == State.Game) {
+                    gp.state = State.Pause;
+                }
+                else if (gp.state == State.Pause) {
+                    gp.state = State.Game;
+                }
+                break;
         }
     }
 
+    /**
+     * interact with key code to do move
+     * 
+     * @param code key code for input key
+     */
+    public void inputMove(int code){
+        // Input for movement in game and triggering Pause screen
+        switch (code) {
+            case KeyEvent.VK_UP:
+                up = true;
+                break;
+            case KeyEvent.VK_DOWN:
+                down = true;
+                break;
+            case KeyEvent.VK_RIGHT:
+                right = true;
+                break;
+            case KeyEvent.VK_LEFT:
+                left = true;
+                break;
+            
+        }
+    }
 
+    /**
+     * interact with key code to do finish game
+     * 
+     * @param code key code for input key
+     */
+    public void inputGameOver(int code){
+        // Input for buttons on GameOver screen
+        if (gp.state == State.Over) {
+            switch (code){
+                case KeyEvent.VK_UP:
+                    gp.ui.order--;
+                    if(gp.ui.order < 0){
+                        gp.ui.order = 1;
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    gp.ui.order++;
+                    if(gp.ui.order > 1){
+                        gp.ui.order = 0;
+                    }
+                    break;
+                case KeyEvent.VK_ENTER:
+                    if(gp.ui.order == 0){
+                        gp.state = State.Game;
+                        gp.retry();
+                    }
+                    if(gp.ui.order == 1){
+                        System.exit(0);
+                    }
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        switch(code){
+            case KeyEvent.VK_UP:
+                up = false;
+                break;
+            case KeyEvent.VK_DOWN:
+                down = false;
+                break;
+            case KeyEvent.VK_RIGHT:
+                right = false;
+                break;
+            case KeyEvent.VK_LEFT:
+                left = false;
+                break;
+        }
+    }
+    
 }
-
-
-
