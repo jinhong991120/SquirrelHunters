d3f435f74112b20d393a0a2fee2b4490f529b4f7
bjs14 <bjs14@asb9700u-i06.csil.sfu.ca>
2023-11-11 18:14:50 -0800

    rm TileType

--- MVN_PJ/phase2-module/src/main/java/com/group11/game/Tile.java
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/Tile.java
@@ -8,9 +8,4 @@ public class Tile{
     /** Attributes of Tile to help represent the map */
     public boolean collision = false;
     public BufferedImage image;
-
-    /** @return int representing TileType */
-    public int getTT(){
-        return TileType;
-    }
 }
