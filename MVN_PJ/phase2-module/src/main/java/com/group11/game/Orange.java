7e07c00cce64c5f505cea7877acbe0dc99e3881a
jla788 <jla788@sfu.ca>
2023-11-12 17:37:46 -0800

    added additional comments to acorn,orange,and potato

--- MVN_PJ/phase2-module/src/main/java/com/group11/game/Orange.java
+++ MVN_PJ/phase2-module/src/main/java/com/group11/game/Orange.java
@@ -1,13 +1,24 @@
 /**
- * extends items class for orange
+ * Class for the orange class 
+ * Images are placed here
  */
 package com.group11.game;
 
+/*
+ * neccesary libraries for images
+ */
 import java.io.IOException;
 import java.awt.Graphics2D;
 import javax.imageio.ImageIO;
 
 public class Orange extends Items{
+    /*
+     * constructor for orange 
+     * sets name to orange
+     * assigns score to 200 
+     * sets collision to true 
+     * grabs image from folder for orange
+     */
     public Orange() {
         name = "Orange"; 
         try {
@@ -19,6 +30,9 @@ public class Orange extends Items{
         score = 200; 
     }
 
+    /*
+     * implementation of draw method 
+     */
     public void draw(Graphics2D g2, GamePanel gp){
     
         g2.drawImage(image, xPosition, yPosition, gp.tileSize, gp.tileSize, null);
