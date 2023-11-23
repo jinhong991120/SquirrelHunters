5b38d4501beffe90f418799fb901ac41858a205a
Jin Hong <jin_hong@sfu.ca>
2023-11-22 16:11:55 -0800

    Create AcornTest.java

---
+++ MVN_PJ/phase2-module/src/test/java/com/group11/AcornTest.java
@@ -0,0 +1,36 @@
+package com.group11;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AcornTest{
+
+    private Items acorn;
+
+    @BeforeEach
+    public void setup(){
+        acorn = new Acorn();
+    }
+
+    @Test // integration testing
+    public void imageTest(){
+        assertNotNull(acorn.image);
+    }
+
+    @Test
+    public void nameTest(){
+        assertEquals(acorn.name, "Acorn");
+    }
+
+    @Test
+    public void scoreTest(){
+        assertEquals(100, acorn.score);
+    }
+
+    @Test
+    public void collisionTest(){
+        assertTrue(acorn.collision);
+    }
+}
