9d5e224db84d8bc6df6f24a695c3f03b1bb42397
Jin Hong <jin_hong@sfu.ca>
2023-11-22 16:10:22 -0800

    Create PotatoTest.java

---
+++ MVN_PJ/phase2-module/src/test/java/com/group11/PotatoTest.java
@@ -0,0 +1,34 @@
+package com.group11;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PotatoTest {
+
+    private Items potato;
+
+    @BeforeEach
+    public void setup(){
+        potato = new Potato();
+    }
+
+    @Test // integration testing
+    public void imageTest(){
+        assertNotNull(potato.image);
+    }
+
+    @Test
+    public void nameTest(){
+        assertEquals(potato.name, "Potato");
+    }
+
+    @Test
+    public void scoreTest(){
+        assertEquals(100, potato.score);
+    }
+
+    @Test
+    public void collisionTest(){assertTrue(potato.collision);}
+}
