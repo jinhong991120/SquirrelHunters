52c03d5025828be8d4d891a1221ddf41bcf6f1a1
Jin Hong <jin_hong@sfu.ca>
2023-11-22 16:13:10 -0800

    Create OrangeTest.java

---
+++ MVN_PJ/phase2-module/src/test/java/com/group11/OrangeTest.java
@@ -0,0 +1,36 @@
+package com.group11;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OrangeTest {
+
+    private Items orange;
+
+    @BeforeEach
+    public void setup(){
+        orange = new Orange();
+    }
+
+    @Test // integration testing
+    public void imageTest(){
+        assertNotNull(orange.image);
+    }
+
+    @Test
+    public void nameTest(){
+        assertEquals(orange.name, "Orange");
+    }
+
+    @Test
+    public void scoreTest(){
+        assertEquals(200, orange.score);
+    }
+
+    @Test
+    public void collisionTest(){
+        assertTrue(orange.collision);
+    }
+}
